4b3fd94d82af914d7a3c59439e7ac3deaf47b2a7
Matheus Souza <129551875+MatheusSouzaSantos7@users.noreply.github.com>
2024-09-17 18:58:28 -0300

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person{
+    private String name;
+    private String address;
+
+    public Person(String name, String address){
+        this.name = name;
+        this. address = address;
+    }
+    
+    // Getters e setters
+    public String getName(){ return name; }
+    public String getAddress(){ return address; }
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Person[name=%s, address=%s]", name, address);
+    }
+
+
+}
